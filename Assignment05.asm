d7e87f435da28831f2bf17b9c0059f1afc536e1d
Ashton Tran <90146460+Whisp00@users.noreply.github.com>
2021-09-05 11:46:43 -0500

    Add files via upload

---
+++ Assignment05.asm
@@ -0,0 +1,114 @@
+;**********************************************************************
+;   This file is a basic code template for assembly code generation   *
+;   on the PIC16F690. This file contains the basic code               *
+;   building blocks to build upon.                                    *  
+;                                                                     *
+;   Refer to the MPASM User's Guide for additional information on     *
+;   features of the assembler (Document DS33014).                     *
+;                                                                     *
+;   Refer to the respective PIC data sheet for additional             *
+;   information on the instruction set.                               *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Filename:	    xxx.asm                                           *
+;    Date:                                                            *
+;    File Version:                                                    *
+;                                                                     *
+;    Author:                                                          *
+;    Company:                                                         *
+;                                                                     * 
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Files Required: P16F690.INC                                      *
+;                                                                     *
+;**********************************************************************
+;                                                                     *
+;    Notes:                                                           *
+;                                                                     *
+;**********************************************************************
+
+
+	list		p=16f690		; list directive to define processor
+	#include	<P16F690.inc>		; processor specific variable definitions
+	
+	__CONFIG    _CP_OFF & _CPD_OFF & _BOR_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_ON & _FCMEN_OFF & _IESO_OFF
+
+
+; '__CONFIG' directive is used to embed configuration data within .asm file.
+; The labels following the directive are located in the respective .inc file.
+; See respective data sheet for additional information on configuration word.
+
+
+
+
+
+
+;***** VARIABLE DEFINITIONS
+COUNT1		EQU 0x20
+COUNT2		EQU	0x21
+
+w_temp		EQU	0x7D			; variable used for context saving
+status_temp	EQU	0x7E			; variable used for context saving
+pclath_temp	EQU	0x7F			; variable used for context saving
+
+
+
+;**********************************************************************
+	ORG		0x000			; processor reset vector
+  	goto		main			; go to beginning of program
+
+
+	ORG		0x004			; interrupt vector location
+	movwf		w_temp			; save off current W register contents
+	movf		STATUS,w		; move status register into W register
+	movwf		status_temp		; save off contents of STATUS register
+	movf		PCLATH,w		; move pclath register into W register
+	movwf		pclath_temp		; save off contents of PCLATH register
+
+
+; isr code can go here or be located as a call subroutine elsewhere
+
+	movf		pclath_temp,w		; retrieve copy of PCLATH register
+	movwf		PCLATH			; restore pre-isr PCLATH register contents	
+	movf		status_temp,w		; retrieve copy of STATUS register
+	movwf		STATUS			; restore pre-isr STATUS register contents
+	swapf		w_temp,f
+	swapf		w_temp,w		; restore pre-isr W register contents
+	retfie					; return from interrupt
+
+
+;******************************************************
+main		
+	banksel	ANSEL
+	clrf	ANSEL
+	banksel	TRISC
+	bcf		TRISC,2
+	bcf		TRISC,5
+	banksel	PORTC
+
+again
+	bsf		PORTC,2
+	bcf		PORTC,5
+	call 	delay
+	bcf		PORTC,2
+	bsf		PORTC,5
+	call	delay
+goto	again
+
+delay:
+LOOP1   DECFSZ  COUNT1      ;subtract 1 from 255 store new count in COUNT	
+		goto	LOOP1			;if COUNT is zero, skip this instruction
+		DECFSZ	COUNT2			;Subtract 1 from 255 	
+		goto 	LOOP1			;go back to start of our loop 
+								;this delay counts down from 255, 255 times, total 65,025
+LOOP2	DECFSZ	COUNT1 	;subtract 1 from 255 store new count in COUNT
+		goto	LOOP2			;if COUNT is zero, skip this instruction
+		DECFSZ	COUNT2		;Subtract 1 from 255 	
+		goto 	LOOP2			;go back to start of our loop
+		
+		
+
+		return	
+end
